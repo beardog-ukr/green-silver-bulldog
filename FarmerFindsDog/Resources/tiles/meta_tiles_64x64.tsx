c1c76a8eb1967aa7a9fca53839dde15a84ecd509
Your Name <you@example.com>
2019-09-10 16:01:30 +0300

    added "meta" tiles for map

---
+++ FarmerFindsDog/Resources/tiles/meta_tiles_64x64.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="meta_tiles" tilewidth="64" tileheight="64" tilecount="2" columns="1">
+ <image source="rg_tiles.png" width="64" height="128"/>
+ <tile id="0">
+  <properties>
+   <property name="Unpassable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="Collectable" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
