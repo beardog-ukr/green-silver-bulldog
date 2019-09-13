659724af315744f64f400c9d8cacb045dcf3e485
Your Name <you@example.com>
2019-09-13 15:20:44 +0300

    Added: initial version of map with advanced meta tiles

---
+++ FarmerFindsDog/Resources/tiles/meta_tiles_64x64_v01.tsx
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="meta_tiles_v01" tilewidth="64" tileheight="64" tilecount="11" columns="11">
+ <properties>
+  <property name="MetaTileId" type="int" value="0"/>
+ </properties>
+ <image source="../../../../mtproj/tileset/z01.png" width="704" height="64"/>
+ <tile id="0">
+  <properties>
+   <property name="MetaTileCode" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="MetaTileCode" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="MetaTileCode" type="int" value="21"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="MetaTileCode" type="int" value="22"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="MetaTileCode" type="int" value="23"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="MetaTileCode" type="int" value="24"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="MetaTileCode" type="int" value="25"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="MetaTileCode" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="MetaTileCode" type="int" value="11"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="MetaTileCode" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="MetaTileCode" type="int" value="12"/>
+  </properties>
+ </tile>
+</tileset>
